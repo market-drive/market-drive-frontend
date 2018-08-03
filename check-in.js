2f716c4d7d8b9ddd08068b41f04aab453c355e9c
Vadim Dashchenko <dashchenko.vad.92@gmail.com>
2018-08-03 17:17:28 +0300

    first commit

---
+++ check-in.js
@@ -0,0 +1,10 @@
+window.onload(
+
+let checkIn = document.querySelector('button');
+
+let userMail = prompt('Ваша почта');
+let userPassword = prompt('Ваш пароль');
+let mailReg = /[A-Za-z]\/@[a-z]{3,4}\.[a-z]{2,3}\d/;
+let passwordReg = /[A-Za-z]\d/ ;
+);
+
