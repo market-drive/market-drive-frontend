7479fc279292f9882ae5b29db293f036801e6dfc
Vadim Dashchenko <dashchenko.vad.92@gmail.com>
2018-10-07 13:06:10 +0300

    commit

--- templates/templates.js
+++ templates/templates.js
@@ -1,19 +1,52 @@
 
-let btn = document.getElementById('create');
-let tr = document.createElement('tr');
-let td = document.createElement('td');
+var btn = document.getElementById('create');
+var res = document.getElementById('template');
 
-//let create = prompt('Введите название шаблона!');
 
-let templates = {};
-function table() {
+var string = [];
+var nameList = document.createElement('span');
+function listCreate() {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+
+    td.classList.add("template");
+    td.style.border = "1px solid black";
+    td.style.borderRadius = "5px";
+    var create = prompt('Введите название шаблона!');
+    //let reg = /[A-Za-z][0-9].*[!@#$%^&*() =+_-]/;
+
     for (var i = 0; i < create.length; i++) {
+            string = create;
+    }
+    nameList.innerHTML = string;
+    td.appendChild(nameList);
+    tr.appendChild(td);
+    res.appendChild(tr);
+    saveList();
+    }
+    btn.addEventListener('click', listCreate);
+
+    function addToList() {
 
     }
+
+    function getList() {
+        goodsList = JSON.parse(localStorage.getItem(string));
+        return goodsList;
+    }
+
+    function saveList() {
+        localStorage.setItem(string, JSON.stringify(goodsList));
+        return string;
+    }
+
+    function add() {
+
+    }
+
+
+function listDelete() {
+    var td = document.getElementsByClassName('template').value;
+    var $this = $(this);
+    td = $this.attr()
 }
-if (create !== null) {
-    templates.push(create);
-} else {
-    console.log('NO');
-}
-btn.addEventListener('click', table);
